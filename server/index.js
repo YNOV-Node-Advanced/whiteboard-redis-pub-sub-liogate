c76ab2f93de73b24edf456fdbb2f1622f964975e
Etienne Fachaux <etienne.fachaux@skyloud.fr>
2018-06-01 16:20:16 +0200

    Server redis

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const uuidv4 = require("uuid/v4");
+const Redis = require("redis");
 
 const app = express();
+const client = Redis.createClient();
+const redisPublisher = Redis.createClient();
+const redisSubscriber = Redis.createClient();
 
 const PUBLIC_FOLDER = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 5000;
@@ -18,12 +22,19 @@ const server = http.createServer(app);
 // Initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
+redisSubscriber.on("message", function(channel, data) {
+  broadcastToSockets(channel, data);
+});
 /*
  * Subscribe a socket to a specific channel.
  */
 function subscribe(socket, channel) {
     let socketSubscribed = socketsPerChannels.get(channel) || new Set();
     let channelSubscribed = channelsPerSocket.get(socket) || new Set();
+
+    if (socketSubscribed.size == 0) {
+        redisSubscriber.subscribe(channel);
+    }
 
     socketSubscribed = socketSubscribed.add(socket);
     channelSubscribed = channelSubscribed.add(channel);
@@ -41,6 +52,10 @@ function unsubscribe(socket, channel) {
 
     socketSubscribed.delete(socket);
     channelSubscribed.delete(channel);
+
+    if (socketSubscribed.size == 0) {
+        redisSubscriber.unsubscribe(channel);
+    }
 
     socketsPerChannels.set(channel, socketSubscribed);
     channelsPerSocket.set(socket, channelSubscribed);
@@ -63,6 +78,8 @@ function unsubscribeAll(socket) {
 function broadcastToSockets(channel, data) {
     const socketSubscribed = socketsPerChannels.get(channel) || new Set();
 
+    // redisPublisher.publish(channel, data);
+
     socketSubscribed.forEach(client => {
         client.send(data);
     });
@@ -76,12 +93,16 @@ wss.on("connection", ws => {
 
     ws.on("message", data => {
         const message = JSON.parse(data.toString());
-
         switch (message.type) {
             case 'subscribe':
+                client.lrange(message.channel, 0, 100, (err, result) => {
+                    result.map(data => ws.send(data));
+                });
                 subscribe(ws, message.channel);
                 break;
             default:
+                client.lpush(message.channel, data);
+                redisPublisher.publish(message.channel, data);
                 broadcastToSockets(message.channel, data);
                 break;
         }
